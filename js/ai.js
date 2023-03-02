47649aa135f7ea8a58fb8112dfb7ced6a8b3946c
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-02 23:27:08 +0600

    add card in html by dynamically

--- js/ai.js
+++ js/ai.js
@@ -3,12 +3,27 @@ const loadData = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    displayData(data.data);
+    displayDatas(data.data.tools);
 }
 
 // const display data
-const displayData = data =>{
-    console.log(data)
+const displayDatas = datas => {
+    const datasContainer = document.getElementById('datas-container');
+    datas.forEach(data => {
+        const dataDiv = document.createElement('div');
+        dataDiv.classList.add('col');
+        dataDiv.innerHTML = `
+        <div class="card">
+            <img src="" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        datasContainer.appendChild(dataDiv);
+    })
 }
 // call the function
 loadData();
