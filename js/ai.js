fd4b751e96ca7e98338e5e898260cdd37dd96531
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-03 19:01:54 +0600

    add spinner

--- js/ai.js
+++ js/ai.js
@@ -1,5 +1,7 @@
 // load data function
 const loadData = async () => {
+    // star spinner
+    toggleSpinner(true);
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
@@ -14,26 +16,42 @@ const displayDatas = datas => {
         dataDiv.classList.add('col');
         dataDiv.innerHTML = `
         <div class="card">
-            <img src="${data.image}" class="card-img-top" alt="">
-            <div class="card-body">
-                <h5 class="card-title fw-bolder mb-3">Features</h5>
-                <p class="card-text mb-0 fs-6 fw-lighter text-secondary">1. ${data.features[0]}</p>
-                <p class="card-text mb-0 fs-6 fw-lighter text-secondary">2. ${data.features[1]}</p>
-                <p class="card-text mb-0 fs-6 fw-lighter text-secondary">3. ${data.features[2]}</p>
-                <hr>
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <h5 class="card-title fw-bolder mb-3">${data.name}</h5>
-                        <p class="card-text mb-0 fs-6 fw-lighter text-secondary"><i class="fa-regular fa-calendar-days"></i> ${data.published_in}</p>
-                    </div>
-                        <!-- card btn -->
-                        <button class="btn btn-outline-danger rounded-circle h-100 my-auto"><i class="fa-solid fa-arrow-right"></i></button>
-                </div>
-            </div>
+        <img src="${data.image}" class="card-img-top img-fluid p-3 rounded-lg" alt="">
+        <div class="card-body">
+        <h5 class="card-title fw-bolder mb-3">Features</h5>
+        <p class="card-text mb-0 fs-6 fw-lighter text-secondary">1. ${data.features[0]}</p>
+        <p class="card-text mb-0 fs-6 fw-lighter text-secondary">2. ${data.features[1]}</p>
+        <p class="card-text mb-0 fs-6 fw-lighter text-secondary">3. ${data.features[2]}</p>
+        <hr>
+        <div class="d-flex justify-content-between">
+        <div>
+        <h5 class="card-title fw-bolder mb-3">${data.name}</h5>
+        <p class="card-text mb-0 fs-6 fw-lighter text-secondary"><i class="fa-regular fa-calendar-days"></i> ${data.published_in}</p>
+        </div>
+        <!-- card btn -->
+        <button class="btn btn-outline-danger rounded-circle h-100 my-auto"><i class="fa-solid fa-arrow-right"></i></button>
+        </div>
+        </div>
         </div>
         `;
         datasContainer.appendChild(dataDiv);
-    })
+    });
+    // stop spinner
+    toggleSpinner(false);
 }
+
+// spinner function section
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none')
+    }
+    else {
+        loaderSection.classList.add('d-none')
+    }
+}
+
+
+
 // call the function
 loadData();
